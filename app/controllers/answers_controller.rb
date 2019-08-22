fa4045df5a786e8987096bb5b05e19b895e0eaf9
wabisachin <wabisachin1122@yahoo.co.jp>
2019-08-22 07:41:50 +0000

    テスト終了後に連続正解５問or合計正解率80%以上かどうか判定し、条件に合うかに応じてcardインスタンスのweaknessの値をを切り替える処理の実装。これによって単語管理画面の苦手克服マークの切り替えが自動で行われる

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -15,7 +15,25 @@ class AnswersController < ApplicationController
             end
             
             Answer.create(card_id: answer["id"], result: answer["result"])
-            # ここから苦手判定を行う処理
+            # ここからテストの結果を受けて苦手判定を行う処理(cardインスタンスのweakpointカラムの数値0or1を決定)
+            # countNumに直近何回分のデータを取ってくるかの数値を代入
+            countNum = 5
+            card = Card.find(answer["id"])
+            # cardモデルのインスタンスメソッドcorrect_rateから正答率を取得
+            correct_rate = card.correct_rate
+            # 同じくcardモデルが持つcorrect_times_counterメソッドから直近の正解数を取得
+            collect_times = card.correct_times_counter(countNum)
+            # ここから条件分岐。合計正答率80%以上または直近連続正解５回で苦手チェックを外す。下回れば苦手チェック
+            if correct_rate >= 0.8 or collect_times == countNum
+                card.weakness = 1
+                card.save
+                # binding.pry
+            else
+                card.weakness = 0
+                card.save
+                # binding.pry
+            end
+            
             
             # binding.pry
         end
